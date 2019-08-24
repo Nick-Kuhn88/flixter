b23f71b0c4474707af6afd00f2428fab6592a14d
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-08-24 01:22:38 +0000

    Added carrierwave and set image upload to s3. working in dev env

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -32,6 +32,6 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def course_params
-    params.require(:course).permit(:title, :description, :cost)
+    params.require(:course).permit(:title, :image, :description, :cost)
   end
 end
