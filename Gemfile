fbf354a60a8e7268f9208dea29ae9acc2f0b2004
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-08-27 03:20:39 +0000

    Added stripe to take credit cards. all enrollment functions are working

--- Gemfile
+++ Gemfile
@@ -68,6 +68,7 @@ gem 'figaro'
 gem "fog-aws"
 gem "mini_magick"
 gem 'carrierwave-aws'
+gem 'stripe'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,17 @@
 class LessonsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   def show
 
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to courses_path, alert: 'You are not enrolled in this course.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
