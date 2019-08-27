e72c9bc29cc870ca1e0950c1f3136bb45478ad8d
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-08-27 01:29:30 +0000

    Added logic in DB that enabled student to enroll in a course and then have the show page be changed based whether or not that student was enrolled

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :views
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end 
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
