29de71dcc4e3072b99eff77692134409e78aac00
Lunkalarm88 <nick.kuhn88@gmail.com>
2019-09-09 00:35:21 +0000

    Completed flixter. All links in footer are operational

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,13 @@ class StaticPagesController < ApplicationController
   
   def index
   end 
+
+  def privacy
+  end
+
+  def team
+  end
+
+  def career
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'career', to: 'static_pages#career'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end 
